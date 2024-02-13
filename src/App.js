6c536cf7368dde19954b65854b4199e97f1d72f0
nikitrawin <tnvjinr@gmail.com>
2024-02-13 22:55:28 +0300

    start refactoring

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
 import "./styles/main.css";
 
+import vk from "./img/icons/vk.svg";
+import instagram from "./img/icons/instagram.svg";
+import twitter from "./img/icons/twitter.svg";
+import gitHub from "./img/icons/gitHub.svg";
+import linkedIn from "./img/icons/linkedIn.svg";
+
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/Header/Header";
+
 function App() {
   return (
 <div>
-    <nav className="nav">
-        <div className="container">
-            <div className="nav-row">
-                <a href="./index.html" className="logo"><strong>Freelancer</strong> portfolio</a>
 
-                <button className="dark-mode-btn">
-                    <img src="./img/icons/sun.svg" alt="Light mode" className="dark-mode-btn__icon"/>
-                    <img src="./img/icons/moon.svg" alt="Dark mode" className="dark-mode-btn__icon"/>
-                </button>
-
-                <ul className="nav-list">
-                    <li className="nav-list__item"><a href="./index.html" className="nav-list__link nav-list__link--active">Home</a></li>
-                    <li className="nav-list__item"><a href="./projects.html" className="nav-list__link">Projects</a></li>
-                    <li className="nav-list__item"><a href="./contacts.html" className="nav-list__link">Contacts</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <header className="header">
-        <div className="header__wrapper">
-            <h1 className="header__title">
-                <strong>Hi, my name is <em>Yuri</em></strong><br/>
-                a frontend developer
-            </h1>
-            <div className="header__text">
-                <p>with passion for learning and creating.</p>
-            </div>
-            <a href="#!" className="btn">Download CV</a>
-        </div>
-    </header>
+    <Navbar />
+    <Header />
 
     <main className="section">
         <div className="container">
@@ -56,11 +37,11 @@ function App() {
         <div className="container">
             <div className="footer__wrapper">
                 <ul className="social">
-                    <li className="social__item"><a href="#!"><img src="./img/icons/vk.svg" alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/instagram.svg" alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/twitter.svg" alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/gitHub.svg" alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src="./img/icons/linkedIn.svg" alt="Link"/></a></li>
+                    <li className="social__item"><a href="#!"><img src={vk} alt="Link"/></a></li>
+                    <li className="social__item"><a href="#!"><img src={instagram} alt="Link"/></a></li>
+                    <li className="social__item"><a href="#!"><img src={twitter} alt="Link"/></a></li>
+                    <li className="social__item"><a href="#!"><img src={gitHub} alt="Link"/></a></li>
+                    <li className="social__item"><a href="#!"><img src={linkedIn} alt="Link"/></a></li>
                 </ul>
                 <div className="copyright">
                     <p>© 2022 frontend-dev.com</p>
